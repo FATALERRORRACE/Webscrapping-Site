b283fcf0a1310086c860989cbacf5804c80fe689
FATALERRORRACE <tfrdrummer@gmail.com>
2020-08-05 19:15:45 -0500

    advance in api

--- config/config.js
+++ config/config.js
@@ -10,7 +10,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || "false";
 
 let urlMongo = "mongodb+srv://SudoN0id:1997Bravo_@pablocluster.o9ynh.gcp.mongodb.net/test";
 
-if(process.env.NODE_ENV === "false"){
-    urlMongo = "mongodb://localhost:27017/DBtest";
-}
+//if(process.env.NODE_ENV === "false"){
+ //   urlMongo = "mongodb://localhost:27017/DBtest";
+//}
 process.env.URLDB = urlMongo;
