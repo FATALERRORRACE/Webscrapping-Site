03e984d3c7339e424d4dd5148939cb2fb95db590
FATALERRORRACE <tfrdrummer@gmail.com>
2020-08-05 19:19:34 -0500

    advance in api

--- config/config.js
+++ config/config.js
@@ -6,11 +6,11 @@ process.env.PORT = process.env.PORT || 3000;
 /* 
  DB CONNECTION
  */
-process.env.NODE_ENV = process.env.NODE_ENV || "false";
+process.env.NODE_ENV =(process.env.NODE_ENV == "production") ? "production" : "false";
 
 let urlMongo = "mongodb+srv://SudoN0id:1997Bravo_@pablocluster.o9ynh.gcp.mongodb.net/test";
 
-//if(process.env.NODE_ENV === "false"){
- //   urlMongo = "mongodb://localhost:27017/DBtest";
-//}
+if(process.env.NODE_ENV === "false"){
+    urlMongo = "mongodb://localhost:27017/DBtest";
+}
 process.env.URLDB = urlMongo;
